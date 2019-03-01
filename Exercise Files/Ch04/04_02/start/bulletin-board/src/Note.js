b04e4846da22b42902f7d999357d077b5c990f45
Anand Angalig <anandangalig@gmail.com>
2019-03-01 12:31:46 -0500

    setting initial state in constructor, then resetting it with method, plus conditional render based on said state

--- Exercise Files/Ch04/04_02/start/bulletin-board/src/Note.js
+++ Exercise Files/Ch04/04_02/start/bulletin-board/src/Note.js
@@ -1,30 +1,62 @@
 import React, { Component } from 'react'
-import FaPencil from 'react-icons/lib/fa/pencil'
-import FaTrash from 'react-icons/lib/fa/trash'
 
 class Note extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			editing: false // setting initial state
+		}
 		this.edit = this.edit.bind(this)
 		this.remove = this.remove.bind(this)
+		this.save = this.save.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
 	}
 	edit() {
-		alert('editing note')
+		this.setState({
+			editing: true // re-setting state when edit() is invoked
+		})
 	}
+
 	remove() {
 		alert('removing note')
 	}
-	render() {
+
+	save() {
+		alert('saved!')
+	}
+
+	renderForm() {
+		return (
+			<div className="note">
+				<form>
+					<textarea />
+					<button onClick={this.save}>Save</button>
+				</form>
+			</div>
+		)
+	}
+
+	renderDisplay() {
 		return (
 			<div className="note">
 				<p>Learn React</p>
 				<span>
-					<button onClick={this.edit} id="edit"><FaPencil /></button>
-					<button onClick={this.remove} id="remove"><FaTrash /></button>
+					<button onClick={this.edit} id="edit">Edit</button>
+					<button onClick={this.remove} id="remove">Remove</button>
 				</span>
 			</div>
 		)
 	}
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay() // conditional rendering of markup based on state
+	}
+
 }
 
-export default Note
+export default Note;
+
+
+
+
+
