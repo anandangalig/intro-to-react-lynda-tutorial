251103d464fa44e78a6a6b9f8dd231c8d31e6ed0
Anand Angalig <anandangalig@gmail.com>
2019-03-01 11:54:01 -0500

    understanding class constructor VS. prototype

--- Exercise Files/Ch04/04_01/start/bulletin-board/src/Note.js
+++ Exercise Files/Ch04/04_01/start/bulletin-board/src/Note.js
@@ -4,36 +4,41 @@ class Note extends React.Component {
 
     constructor() {
         super();
-        this.edittt = this.edit.bind(this);
+        // this.edittt = this.edit.bind(this);
+        // constructors gets called with each new instance, and sets the properties of the new object that are given here
+        this.edit = function(){
+            alert('from constructor!');
+        }
         this.removeee = this.remove.bind(this);
         // The first this.edit refers to a property that will be created in every instance, because its being set in the constructor. The second this.edit refers to an already existing property living in the prototype object of the class. 
         // This connection allows the instantiated Note to call the methods in the prototype, which are separate.
     }
 
-    // ===== included in prototype ===================
+    // ===== included in prototype, separate from constructor(): ===================
+    // thus these are accessible from the instance as the parent/prototypal reference
     edit() {
-        alert('editing...');
+        alert('from prototype!');
         console.log(this); // this == undefined unless: this.remove = this.remove.bind(this); in the constructor        
     }
     remove() {
         alert('removing...');
         console.log(this);
     }
-    // ===== End included in prototype ===================
-
-    // 
     render() {        
         return(
             <div className="note">
                 <p>Learn React</p>
                 <span>
                     {/* onClick={this.edit} is treated as callback */}
-                    <button id='edit' onClick={this.edittt}>Edit</button> 
+                    <button id='edit' onClick={this.edit}>Edit</button> 
                     <button id='remove' onClick={this.removeee}>Remove</button>
                 </span>
             </div>
         );  
     }
 }
+
+let test = new Note();
+test.edit();
 
 export default Note;
